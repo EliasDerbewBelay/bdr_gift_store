9b86ff04c67ffc471a70abce32c38f9de2cf8564
EliasDerbewBelay <eliasderbew1@gmail.com>
2026-04-16 23:03:51 +0300

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import "./globals.css";
 import { Providers } from "@/app/providers";
 import { GuardProvider } from "@/context/GuardContext";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
 
 export default function RootLayout({
   children,
@@ -8,7 +13,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning className={cn("font-sans", geist.variable)}>
       <body>
         <Providers>
           <GuardProvider>{children}</GuardProvider>
